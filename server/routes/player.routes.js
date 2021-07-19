874bc0bc597fb581331884b4e7672ecd7c640325
MaganaOscar <oscmag13@gmail.com>
2021-07-19 10:29:08 -0700

    added routes

--- server/routes/player.routes.js
+++ server/routes/player.routes.js
@@ -1,5 +1,9 @@
 const PlayerController = require("../controllers/player.controller");
 
 module.exports = app => {
-    
+    app.post('/api/player', PlayerController.createPlayer);
+    app.get('/api/player', PlayerController.findAllPlayers);
+    app.get('/api/player/:id', PlayerController.getPlayer);
+    app.put('/api/player/:id', PlayerController.updatePlayer);
+    app.delete('/api/player/:id', PlayerController.deletePlayer);
 }
